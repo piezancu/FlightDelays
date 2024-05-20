083dee03ae1f8c86d2f057b970d40f7d5ba27f1c
Giuseppe Averta <g.averta3@gmail.com>
2024-05-20 11:34:28 +0200

    add method for nodes

--- database/DAO.py
+++ database/DAO.py
@@ -21,3 +21,29 @@ class DAO():
         cursor.close()
         conn.close()
         return result
+
+    @staticmethod
+    def getAllNodes(Nmin):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """select tmp.ID, tmp.IATA_CODE, count(*) as N
+                    from (
+                    SELECT a.ID , a.IATA_CODE , f.AIRLINE_ID, count(*) as n
+                    FROM airports a , flights f 
+                    WHERE a.ID = f.ORIGIN_AIRPORT_ID or a.ID = f.DESTINATION_AIRPORT_ID 
+                    group by a.ID , a.IATA_CODE , f.AIRLINE_ID
+                    ) as tmp
+                    group by tmp.ID, tmp.IATA_CODE
+                    having N >= %s"""
+
+        cursor.execute(query, (Nmin,))
+
+        for row in cursor:
+            result.append(row)
+
+        cursor.close()
+        conn.close()
+        return result
